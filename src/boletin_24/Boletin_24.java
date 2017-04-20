1594f39ef0f9d3ed0cc5cae7f6e958a0ea1ef933
MarioBlancosoto <Mario@DESKTOP-NPV8TDI>
2017-04-20 15:54:50 +0200

    commit 1

--- src/boletin_24/Boletin_24.java
+++ src/boletin_24/Boletin_24.java
@@ -6,7 +6,10 @@ public class Boletin_24 {
 
 
     public static void main(String[] args) {
-   
+   Usuario user = new Usuario();
+   user.setVisible(true);
+   Devoluciones dev = new Devoluciones();
+   dev.setVisible(true);
     }
     
 }
